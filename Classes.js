d7e8cdf14f13e953eb1af38bae3ca9fe2f715701
manoo25 <mohamedsalamma25@gmail.com>
2025-05-18 12:57:17 +0300

    The End

--- Classes.js
+++ Classes.js
@@ -1,5 +1,13 @@
-class HelloWorld {
+class HelloJava {
     printHelloWorld() {
         console.log('Hello World!');
+    }
+}
+class HelloIti {
+    printHelloIti() {
+        console.log('Hello Iti!');
+    }
+    printHelloGit() {
+        console.log('Hello Git!');
     }
 }
